2c5a17ba80dde3dd03d11b81b31723508ef75685
Konrad <konrad.domalewski@gmail.com>
2023-05-23 16:43:03 +0200

    changed border

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log('Hello from Nerdbord!')
+const colors = ["#F97316", "#0E9C88", "#9F4200", "#FFAB6F"]
+const box = document.querySelector(".box")
+let currentIndex = 0;
+
+setInterval(() => {
+    let newColor = colors[currentIndex]
+    box.style.borderColor = newColor
+    currentIndex += 1
+    if (currentIndex >= colors.length) {
+        currentIndex = 0
+    }
+}, 3000)
